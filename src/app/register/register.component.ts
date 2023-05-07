a8e6f6f7bc31c991437e35cc145ca7e4d2595239
MEVINDANI <mevindani14@gmail.com>
2023-05-07 14:23:50 +0530

    ngSubmit()

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -6,5 +6,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent {
+  
   welcomeData="Your perfect Banking Partner"
+  accNum:any
+  uname:any
+  passwd:any
+
+  register() {
+    console.log(this.accNum,this.uname,this.passwd);
+    alert("User Registered")
+  }
 }
